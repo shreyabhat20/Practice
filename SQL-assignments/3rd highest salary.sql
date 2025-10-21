82c6cefee5f1fc1d7bf301cdeed6a8d333518685
shreyabhat20 <15shreyabhat@gmail.com>
2025-10-21 16:01:15 +0530

    Add assignments

--- SQL-assignments/3rd highest salary.sql
+++ SQL-assignments/3rd highest salary.sql
@@ -1,15 +1,9 @@
---to find all departments where total salary expenditure is over 500000 and is having more than 10 employees. The result should contain department name and total salary of it
-SELECT 
-    d.dept_name,
-    SUM(e.salary) AS total_salary
-FROM 
-    employees e
-JOIN 
-    departments d 
-ON 
-    e.dept_id = d.dept_id
-GROUP BY 
-    d.dept_name
-HAVING 
-    SUM(e.salary) > 500000
-    AND COUNT(e.emp_id) > 10;
+--lists out the employee names whose salary is the 3rd highest in the company
+SELECT emp_name, salary
+FROM employees
+WHERE salary = (
+    SELECT DISTINCT salary
+    FROM employees
+    ORDER BY salary DESC
+    OFFSET 2 LIMIT 1
+);
